88cea8f8453f59ba9e736c028192c367e9d2667f
Lucia Alonzo <lealonzoc@correo.url.ed.gt>
2026-08-23 23:18:34 -0600

    Se configuro que se pudiera ver el apartado de Correo, la integracion entre el menu y el correo, se agrego un correo destino/remitente, se agregaron mas restricciones para mandar correos y se separo el Footer del Correo

--- backend/correo.routes.js
+++ backend/correo.routes.js
@@ -3,7 +3,6 @@ import correoController from "./correo.controller.js";
 
 const router = express.Router();
 
-router.get("/footer", correoController.obtenerFooter);
 router.post("/contacto", correoController.enviarContacto);
 
 export default router;
